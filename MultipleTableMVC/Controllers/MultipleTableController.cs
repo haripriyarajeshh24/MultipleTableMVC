0c84348a4f933af6a0c1b665565144a889111ba3
Jith Dev <jithdev@xminds.com>
2024-10-16 18:46:56 +0530

    Add project files.

---
+++ MultipleTableMVC/Controllers/MultipleTableController.cs
@@ -0,0 +1,49 @@
+﻿using MultipleTableMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MultipleTableMVC.Controllers
+{
+    public class MultipleTableController : Controller
+    {
+        // GET: MultipleTable
+        public ActionResult Index()
+        {
+            MVCCRUDDBEntities db = new MVCCRUDDBEntities();
+            List<Product> list = db.Products.ToList();
+            ViewBag.ProductList = new SelectList(list, "ProductID", "ProductName");
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Index(ProductViewModel model)
+        {
+            MVCCRUDDBEntities db = new MVCCRUDDBEntities();
+            List<Product> list = db.Products.ToList();
+            ViewBag.ProductList = new SelectList(list, "ProductID", "ProductName");
+
+            Product product = new Product();
+            product.ProductID = model.ProductID;
+            product.ProductName = model.ProductName;
+            product.Category = model.Category;
+            int productid = product.ProductID;
+
+            db.Products.Add(product);
+            db.SaveChanges();
+
+
+            Order order = new Order();
+            order.OrderNumber = model.OrderNumber;
+            order.PersonName = model.PersonName;
+            order.ProductID = productid;
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+
+            return View(model);
+        }
+    }
+}
---
+++ MultipleTableMVC/Models/OrderViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MultipleTableMVC.Models
+{
+    public class OrderViewModel
+    {
+        public int OrderID { get; set; }
+        public int OrderNumber { get; set; }
+        public string PersonName { get; set; }
+        public Nullable<int> ProductID { get; set; }
+        public string ProductName { get; set; }
+        public virtual Product Product { get; set; }
+       
+    }
+
+    public class ProductViewModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+
+        public string Category { get; set; }
+
+        public int OrderNumber { get; set; }
+        public string PersonName { get; set; }
+
+        public int OrderID { get; set; }
+
+    }
+}
